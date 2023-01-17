3365bb4802f8e76f80046b5a556b31bbac8ee8f7
ongbular <ongbular@gmail.com>
2023-01-17 19:29:42 +0900

    Add Engine Module : CrashReporter

---
+++ Module/CrashReporter/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrashReporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var args = Environment.GetCommandLineArgs();
+            Debug.Assert(args.Length == 3);
+
+            LogPath = args[1];
+            CallStack = args[2];
+
+            this.DataContext = this;
+        }
+
+        private void Hyperlink_GitHubRepo(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", e.Uri.AbsoluteUri);
+            e.Handled = true;
+        }
+        public string LogPath { get; set; }
+        public string CallStack { get; set; }
+    }
+}
